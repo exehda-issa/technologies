f313c5f1fb01cbc5b4f4995bfe4d23965ea844d2
EXEHDA-ISSA <31638544+exehda-issa@users.noreply.github.com>
2017-09-18 08:29:53 -0300

    Update ap_client_info.py

--- logstash/ap_client_info.py
+++ logstash/ap_client_info.py
@@ -11,13 +11,13 @@ data_json = []
 output = ''
 
 for winst_server in winst_servers:
-        ret_ap = subprocess.check_output(["ssh", "-o PreferredAuthentications=publickey", "-p 50000", "user@" + winst_server, "cat /root/winst/ap_serv | awk -F';' '{print $2}'"])
+        ret_ap = subprocess.check_output(["ssh", "-o PreferredAuthentications=publickey", "-p 20000", "user@" + winst_server, "cat /root/winst/ap_serv | awk -F';' '{print $2}'"])
         ap_servers = ret_ap.splitlines()
         for ap_server in ap_servers:
                 ap_ip = ap_server
                 with open(os.devnull, 'w') as devnull:
 			try:
-	                        ret = subprocess.check_output(["ssh", "-o PreferredAuthentications=publickey", "-p 50000", "user@" + winst_server, "sshpass -p 'password' ssh -o ConnectTimeout=10 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vendor_login@" + ap_ip + " \"wstalist; mca-status\""], stderr=devnull)
+	                        ret = subprocess.check_output(["ssh", "-o PreferredAuthentications=publickey", "-p 20000", "user@" + winst_server, "sshpass -p 'password' ssh -o ConnectTimeout=10 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vendor_login@" + ap_ip + " \"wstalist; mca-status\""], stderr=devnull)
 			except:
 				e = exc_info()[0]
 				data = {}
